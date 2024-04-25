fd736d880c09e0390ec0cf7dca47fe803319f561
jkamere <kamererjoshual@gmail.com>
2024-04-25 13:37:45 -0400

    project02

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Project 02: RPG: The Liminal Riddler
-You wake up in an unknown hotel and have to solve riddles in each room to make it outm if you fail, you are trapped forever.
+-This is a text based riddle solving game where the player as to enter rooms and grab the item that is associated with the answer.
+-When the game starts, you will be giving a little intro and be thrown into a hallway with a few rooms that you can explore.
+-Just make sure not to get a riddle wrong, because that means game over.
+-Note: Enter the names of the items exactly as they are listed
--- project02.py
+++ project02.py
@@ -4,6 +4,8 @@
 #Github Link: https://github.com/Jkamere/Project-02---RPG
 #Description: Riddle solving game
 
+import logging
+
 #class for when exit not found error
 class ExitNotFoundError(Exception):
     def __init__(self, room_name, message="Room not found"):
@@ -64,13 +66,29 @@ class AdventureMap:
             raise ExitNotFoundError(room_name)
         return self.map[room_name_lower]
 
+def setup_logger():
+    logger = logging.getLogger('adventure_game_logger')
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler('adventure_game.log')
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
 
 def main():
+    logger = setup_logger()
+    
     adventure_map = AdventureMap()
+
     adventure_map.add_room(Room("Hallway", "A long, unsettling hallway that has 3 rooms, room 901, room 902, room 903, and a dimly lit elevator at the end.", ['Room 901', 'Room 902', 'Room 903', 'Elevator']))
-    adventure_map.add_room(Room("Room 901", "You open the door and enter the room and instantly you see a man in, what you think is, a medieval jester costume telling you incomprehensible riddles and countless other items laying around.", ["Hallway"], [Item("An annoying riddler", "you just wish he would shut up"), Item("A toy spider", "At least you hope its a toy"), Item("A book", "It is titled 'Sweet Dreams'"), Item("Statue of the Letter", "Statue of a letter E for some reason")]))
-    adventure_map.add_room(Room("Room 902", "As you open the door, you see a destroyed hotel room, as if a tornado went through it. But through the rubble, you see a few items that aren't destroyed and ones that stick out.", ["Hallway"], [Item("A book", "It is called 'Temper Management'"), Item("A plastic tongue", "Please be plastic..."), Item("A sword", "It is sharpened and has a nice shine to it"), Item("A portrait", "Of everything destroyed, a painting of Jesus remains on the wall")]))
-    adventure_map.add_room(Room("Room 903", "As you enter the room, you see that it looks nothing like a hotel room. There is sand every, some small trees and potted bushes, and a light up moon hanging from the ceiling.", ["Hallway"], [Item("A sandbox", "It has a castle and a little town in it"), Item("The moon", "A small lamp moon thats hanging from the ceiling"), Item("A map", "There is a map of some unknown place on the wall"), Item("An Idea", "There is a box that is labelled, think tank")]))
+    adventure_map.add_room(Room("Room 901", "You open the door and enter the room and instantly you see a man in, what you think is, a medieval jester costume telling you incomprehensible riddles and countless other items laying around.", ["Hallway", 'Elevator'], [Item("An annoying riddler", "you just wish he would shut up"), Item("A toy spider", "At least you hope its a toy"), Item("A book", "It is titled 'Sweet Dreams'"), Item("Statue of the Letter E", "Statue of a letter E for some reason")]))
+    adventure_map.add_room(Room("Room 902", "As you open the door, you see a destroyed hotel room, as if a tornado went through it. But through the rubble, you see a few items that aren't destroyed and ones that stick out.", ["Hallway", 'Elevator'], [Item("A book", "It is called 'Temper Management'"), Item("A plastic tongue", "Please be plastic..."), Item("A sword", "It is sharpened and has a nice shine to it"), Item("A portrait", "Of everything destroyed, a painting of Jesus remains on the wall")]))
+    adventure_map.add_room(Room("Room 903", "As you enter the room, you see that it looks nothing like a hotel room. There is sand every, some small trees and potted bushes, and a light up moon hanging from the ceiling.", ["Hallway", 'Elevator'], [Item("A sandbox", "It has a castle and a little town in it"), Item("The moon", "A small lamp moon thats hanging from the ceiling"), Item("A map", "There is a map of some unknown place on the wall"), Item("An Idea", "There is a box that is labelled, think tank")]))
     adventure_map.add_room(Room("Elevator", "When you get to the elevator and press the only button on it, down, nothing happens, it doesn't open, doesn't call an elevator, and you can't even pry it open with your hands.", ["Hallway", 'Room 901', 'Room 902', 'Room 903']))
 
     current_room = adventure_map.get_room("Hallway")
@@ -102,7 +120,6 @@ def main():
                 if choice == "no":
                     continue
             elif current_room.name.lower() == "room 902" and not entered_room_902:
-                # Enter room 902 logic here
                 print("A riddle appears in front of you")
                 print("'I'm rarely touched but often held. If you have wit, you'll use me well.'")
                 entered_room_902 = True
@@ -114,7 +131,6 @@ def main():
                 if choice == "no":
                     continue
             elif current_room.name.lower() == "room 903" and not entered_room_903:
-                # Enter room 903 logic here
                 print("A riddle appears in front of you")
                 print("'I have seas with no water, coasts with no sand, towns without people, mountains without land. What am I?'")
                 entered_room_903 = True
@@ -128,9 +144,12 @@ def main():
             elif current_room.name.lower() != "room 901" and current_room.name.lower() != "room 902" and current_room.name.lower() != "room 903":
                 continue
 
+
+
+
             item_choice = input("Which item do you pick up?\n").strip().lower()
             if current_room.name.lower() == "room 901" and entered_room_901 and entered_room_902 and entered_room_903:
-                correct_item_names4 = ["statue of the letter e", "statue", "e"]
+                correct_item_names4 = ["statue of the letter e"]
                 if item_choice in correct_item_names4:
                     print('As soon as you grab it, you hear a DING! You look out the door, the elevator opened! You sprint for it and just make it inside, you made it out!')
                     break
@@ -138,27 +157,30 @@ def main():
                     print("The door slams shut, and you run to it! You aren't able to open it! You realize you are trapped there forever.... GAME OVER!")
                     break
             elif current_room.name.lower() == "room 901" and entered_room_901:
-                correct_item_names = ["a book", "book",]
+                correct_item_names = ["a book"]
                 if item_choice in correct_item_names:
                     print("After grabbing the item, a new note appears that reads, 'Congrats, you have beat the riddle. Now onto the next!'")
                     inventory.append(item_choice)
+                    current_room.items = [item for item in current_room.items if item.name.lower() != item_choice]
                 else:
                     print("The door slams shut, and you run to it! You aren't able to open it! You realize you are trapped there forever.... GAME OVER!")
                     break
                     inventory.append(item_choice)
             elif current_room.name.lower() == "room 902" and entered_room_902:
-                correct_item_names2 = ["tongue", "a tongue", "a plastic tongue"]
+                correct_item_names2 = ["a plastic tongue"]
                 if item_choice in correct_item_names2:
                     print("After grabbing the item, a new note appears that reads, 'Congrats, you have beat the riddle Now onto the next!'")
                     inventory.append(item_choice)
+                    current_room.items = [item for item in current_room.items if item.name.lower() != item_choice]
                 else:
                     print("The door slams shut, and you run to it! You aren't able to open it! You realize you are trapped there forever.... GAME OVER!")
                     break
             elif current_room.name.lower() == "room 903" and entered_room_903:
-                correct_item_names3 = ["map", "a map"]
+                correct_item_names3 = ["a map"]
                 if item_choice in correct_item_names3:
                     print("After grabbing the item, a new note appears that reads, 'Congrats, you have beat the riddle. Now onto the next!'")
                     inventory.append(item_choice)
+                    current_room.items = [item for item in current_room.items if item.name.lower() != item_choice]
                 else:
                     print("The door slams shut, and you run to it! You aren't able to open it! You realize you are trapped there forever.... GAME OVER!")
                     break
@@ -168,12 +190,10 @@ def main():
         else:
             print(f"{choice} -> That isn't connected to this area")
         
-        # Display player's inventory
         print("Your Inventory:")
         for item in inventory:
             print("- " + item)
         
-        # Check if all rooms have been visited
         if entered_room_901 and entered_room_902 and entered_room_903:
             print("After reading the note, a voice starts talking in your head! 'You seem to have made it past all 3 rooms... you should head back to the elevator'")
             adventure_map.get_room("elevator").description = "The voice in your head speaks again,\n'Grab the note from in between the doors, I have 1 final riddle for you, then you will be free, and no choices this time though... What is the beginning of eternity, the end of time and space, the beginning of every end and the end of every place.'"
